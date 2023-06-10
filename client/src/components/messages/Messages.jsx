dde5624499b43eb58fd8276c6ef611ad046baf68
Julie Krav <j.krav07@gmail.com>
2023-06-10 15:43:10 -0700

    React Native Navigation setup

--- client/src/components/messages/Messages.jsx
+++ client/src/components/messages/Messages.jsx
@@ -1,9 +1,13 @@
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import NavigationPane from '../NavigationPane';
 
 export default function Messages() {
   return (
+
     <View>
       <Text> This is the messages main page</Text>
     </View>
-  )
+
+  );
 }
--- client/src/components/record-catalog/RecordCatalog.jsx
+++ client/src/components/record-catalog/RecordCatalog.jsx
@@ -1,9 +1,12 @@
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import NavigationPane from '../NavigationPane';
 
 export default function RecordCatalog() {
   return (
+
     <View>
       <Text> This is the Record Catalog main page</Text>
     </View>
-  )
+  );
 }
