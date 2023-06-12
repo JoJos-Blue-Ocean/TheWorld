14644852bcbd4773efa72b0f8a2cf9587c0a281f
Tony Truong <Tonytruong0698@gmail.com>
2023-06-11 19:28:03 -0700

    update

--- client/src/components/trading-history/TradingHistory.jsx
+++ client/src/components/trading-history/TradingHistory.jsx
@@ -1,16 +1,52 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-<<<<<<< HEAD
-import Trades from './Trades';
-=======
+import React, { useState } from 'react';
+import {
+  StyleSheet, Text, View, Dimensions,
+} from 'react-native';
+import YourListing from './YourListing';
 import NavigationPane from '../NavigationPane';
->>>>>>> main
+
+const { width: screenWidth, height: screenHeight } = Dimensions.get('window');
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    top: '10%',
+    backgroundColor: '#F5F5F5',
+  },
+  tabs: {
+    position: 'absolute',
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  tab: {
+    position: 'relative',
+    backgroundColor: 'grey',
+    padding: '5%',
+  },
+  tradesHistoryMain: {
+    position: 'absolute',
+    backgroundColor: 'grey',
+    flex: 1,
+  },
+});
 
 export default function TradingHistory() {
+  const [tab, setTab] = useState('Your Listing');
+
   return (
 
-    <View>
-
+    <View style={styles.main}>
+      <View style={styles.tabs}>
+        <View style={styles.tab} onClick={() => { setTab('Your Listing'); }}>
+          <Text>Your Listing</Text>
+        </View>
+        <View style={styles.tab} onClick={() => { setTab('Transaction History'); }}>
+          <Text>Transaction History</Text>
+        </View>
+      </View>
+      <View style={styles.tradesHistoryMain}>
+        <YourListing />
+      </View>
     </View>
 
   );
---
+++ client/src/components/trading-history/YourListing.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function YourListing() {
+  return (
+    <View style={styles.trades}>
+      <Text>this is your listing</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  trades: {
+    position: 'absolute',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+});
