cd2b0be46c1ed14c025aa7975392996cae4eab48
Segue <gencogluseguy@gmail.com>
2023-06-12 18:06:16 -0700

    Wishlist implementation wip

--- server/routes/helpers.js
+++ server/routes/helpers.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const helpers = require('../helpers');
 
+console.log('is routes helpers.js running?');
 router.get('', helpers.getAlbums);
 
 module.exports = router;
