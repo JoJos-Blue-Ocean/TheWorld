5098c557280b1b141ff80753832e25492036e881
davidguy3237 <davidtran3237@gmail.com>
2023-06-13 14:48:33 -0700

    Updates to messages schema

--- server/database/schemas/messages-seed.sql
+++ server/database/schemas/messages-seed.sql
@@ -1,11 +1,11 @@
-INSERT INTO messages(sender_id, recipient_id, trade_id, body)
-VALUES('cliuo1dcs000208i9hga217k5', 'cliunrnur000008ms1u5qhzl2', 1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
-INSERT INTO messages(sender_id, recipient_id, trade_id, body)
-VALUES('cliunrnur000008ms1u5qhzl2', 'cliuo1dcs000208i9hga217k5', 1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
-INSERT INTO messages(sender_id, recipient_id, trade_id, body)
-VALUES('cliuo1dcs000208i9hga217k5', 'cliunrnur000008ms1u5qhzl2', 1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
-INSERT INTO messages(sender_id, recipient_id, trade_id, body)
-VALUES('cliuo1dcs000208i9hga217k5', 'cliunrnur000008ms1u5qhzl2', 1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
+INSERT INTO messages(sender_id, recipient_id, body)
+VALUES('cliuo1dcs000208i9hga217k5', 'cliunrnur000008ms1u5qhzl2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
+INSERT INTO messages(sender_id, recipient_id, body)
+VALUES('cliunrnur000008ms1u5qhzl2', 'cliuo1dcs000208i9hga217k5', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
+INSERT INTO messages(sender_id, recipient_id, body)
+VALUES('cliuo1dcs000208i9hga217k5', 'cliunrnur000008ms1u5qhzl2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
+INSERT INTO messages(sender_id, recipient_id, body)
+VALUES('cliuo1dcs000208i9hga217k5', 'cliunrnur000008ms1u5qhzl2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.')
 
 -- Because of timestamps, if you just run this script, they'll all have the same timestamp
 -- So you're going to have to enter each one manually
