a2b34d62de494eb834b5c7e7d1cf3d21b474803c
Segue <gencogluseguy@gmail.com>
2023-06-17 18:53:44 -0700

    sql file added uid

--- server/database/schemas/wishlistTest.sql
+++ server/database/schemas/wishlistTest.sql
@@ -1,7 +1,7 @@
 INSERT INTO wishlist(user_id, artist_name, album_id,album_name, genre, image)
 VALUES( 'a', 'SanFrancisco', '6', 'balala', 'rock', 'httadasdas.com');
 INSERT INTO wishlist(user_id, artist_name, album_id, album_name, genre, image)
-VALUES( 'b', 'London', '4', 'artist', 'electronic', 'holacoasdasd.com');
+VALUES( 'Wz0NzFeqFeXxmnKwo3jR9TaWu2v2', 'London', '4', 'artist', 'electronic', 'holacoasdasd.com');
 INSERT INTO wishlist(user_id, artist_name, album_id, album_name, genre, image)
 VALUES( 'c', 'Paris', '8', 'test', 'jazz', 'https.//asdas.com');
 INSERT INTO wishlist(user_id, artist_name, album_id,album_name, genre, image)
